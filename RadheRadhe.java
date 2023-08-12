01db93f606ab08785dcfebf97f4a9af2190538a0
roshanvarma007 <roshanvarma352@gmail.com>
2023-08-12 11:17:15 +0530

    Change foloder

--- RadheRadhe.java
+++ RadheRadhe.java
@@ -1,21 +1,6 @@
 public class RadheRadhe {
     public static void main(String[] args) {
-
-      String name = "Radha";
-   String love = "Krishna";
-
-
-
-//
-////        System.out.println(name.compareTo(love));
-////        System.out.println(love.length());
-//////        System.out.println(love.charAt(5));
-////        System.out.println(love.describeConstable());
-////        System.out.println(love.formatted(love));
-//        System.out.println(name.indexOf());
-
-
-
+        System.out.println("Test");
     }
 
 
